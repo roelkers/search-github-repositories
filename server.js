4639bf1253053868b7ca3f89fdf88dfa860913fe
roelkers <rufus.oelkers@gmail.com>
2019-04-01 13:32:51 +0200

    fix typo in server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const gitHubApi = require('./githubApi');
+const githubApi = require('./githubApi');
 const app = express();
 const port = process.env.PORT || 5000;
 
